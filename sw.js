d4b5fbc8ce7bed270c75897815f0db2569cb138f
iftrebor <robert.plietzsch@icloud.com>
2026-03-08 20:37:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME='gravel-plan-v61-cache';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));});
